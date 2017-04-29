c0e4db8b3d5a7c98260eb8ee5df1bc95002ed9d8
iwata <iwaking43@gmail.com>
2017-04-29 00:17:46 +0000

    ranking have

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,13 @@
 class RankingsController < ApplicationController
+
   def want
     @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
   end
+
+  def have
+    @ranking_counts_have = Have.ranking
+    @items = Item.find(@ranking_counts_have.keys)
+  end
+
 end
